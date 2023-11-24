c2f8c7abf4db9487f425a821c8519989e2144c53
mdinalam <mdinalam@LIN83002015.corp.capgemini.com>
2023-11-24 20:48:43 +0530

    DAY 5

--- springboot-restful-webservices/src/main/java/com/cg/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/com/cg/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
@@ -5,7 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title ="Spring Boot Rest Api Documentation",
+				description = "Spring Boot Rest Api Documentation",
+				version = "v1.0",
+				license = @License(
+						name = "Apache 2.0"
+								)
+				)
+				
+		)
+		
+		
 public class SpringbootRestfulWebservicesApplication {
 
 	public static void main(String[] args) {
--- springboot-restful-webservices/src/main/java/com/cg/springbootrestfulwebservices/controller/UserController.java
+++ springboot-restful-webservices/src/main/java/com/cg/springbootrestfulwebservices/controller/UserController.java
@@ -17,9 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cg.springbootrestfulwebservices.dto.UserDto;
 import com.cg.springbootrestfulwebservices.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
+
+@Tag(
+	name = "CRUD REST APIs for User Resource",
+	description = "CRUD REST APIs - Create User, Update user, Get User, Get All Users, Delete Users"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -28,30 +36,56 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Operation(summary = "Create User REST API",
+			description = "Create User REST API is used to save user in database.")
+	@ApiResponse(
+			responseCode = "201",
+			description = "HTTP Status 201 CREATED"
+			)
 	@PostMapping
 	public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto user){
 		UserDto savedUser = userService.createUser(user);
 		return new ResponseEntity<UserDto>(savedUser, HttpStatus.CREATED);
 	}
-	
+	@Operation(summary = "Get User BY ID REST API",
+			description = "Get User By Id REST API is used to get single user from database.")
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+			)
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
 		UserDto user = userService.getUserById(id);
 		return new ResponseEntity<UserDto>(user, HttpStatus.OK);
 	}
-	
+	@Operation(summary = "Get all Users REST API",
+			description = "Get all Users REST API is used to get all users from database.")
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+			)
 	@GetMapping
 	public ResponseEntity<List<UserDto>> getAllUsers(){
 		List<UserDto> users = userService.getAllUsers();
 		return new ResponseEntity<List<UserDto>>(users,HttpStatus.OK);
 	}
-	
+	@Operation(summary = "Update User by id REST API",
+			description = "Update User By Id REST API is used to update single user in the database.")
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+			)
 	@PutMapping("/{id}")
 	public ResponseEntity<UserDto> updateUser(@RequestBody @Valid UserDto user) {
 		UserDto updatedUser = userService.updateUser(user);
 		return new ResponseEntity<UserDto>(updatedUser, HttpStatus.OK);
 	}
-	
+	@Operation(summary = "Delete User BY ID REST API",
+			description = "Delete User By Id REST API is used to delete single user from database.")
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+			)
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteUser(@PathVariable Long id){
 		userService.deleteUser(id);
